ac2a305a45d24163d1413f596e08406e0f2ab43b
Jesus Albino <jesusalbino@MacBook-Air-de-Jesus.local>
2025-12-01 08:59:54 -0600

    fix: Db path to relative folder

--- app/depends.py
+++ app/depends.py
@@ -1,9 +1,10 @@
+import os
+
 import sqlite3
 
-db_path = "/Users/jesusalbino/Projects/kavak/commercial-ai-agent/example.db"
+data_path = os.path.join(os.getcwd(), "data")
+db_path = os.path.join(data_path, "example.db")
 agents_db_conn = sqlite3.connect(db_path, check_same_thread=False)
 
-car_catalog_db_path = (
-    "/Users/jesusalbino/Projects/kavak/commercial-ai-agent/car_catalog.db"
-)
+car_catalog_db_path = os.path.join(data_path, "car_catalog.db")
 car_catalog_db_conn = sqlite3.connect(car_catalog_db_path, check_same_thread=False)
